c3d458d01b98a9541bc9890b5256e3203042f314
firoliyos <firatesfaye44@gmail.com>
2024-10-21 10:48:17 +0300

    menu-item changed

--- recursive-nav-menu/src/components/menu-item.jsx
+++ recursive-nav-menu/src/components/menu-item.jsx
@@ -1,5 +1,34 @@
+import { useState} from "react"
+import MenuList from "./menu-list"
+
 export default function MenuItem({item}) {
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({})
+
+   function handleToggleChildren(getCurrentLabel) {
+     setDisplayCurrentChildren({
+        ...displayCurrentChildren,
+        [getCurrentLabel] : !displayCurrentChildren[getCurrentLabel]
+     })
+   }
+
+   console.log(displayCurrentChildren)
+
     return(
-      <div>Menu Item</div>  
+      <li>
+       <div style={{display: 'flex', gap: '20px'}} >
+         <p>{item.label}</p>
+         {
+           item && item.children && item.children.length ? 
+           <span onClick={() => handleToggleChildren(item.label)} > 
+             + 
+           </span> : null 
+         }
+       </div>
+       {
+        item && item.children && item.children.length > 0 && displayCurrentChildren[item.label] ? 
+        <MenuList list={item.children} />
+        : null
+       }
+      </li>  
     )
 }
