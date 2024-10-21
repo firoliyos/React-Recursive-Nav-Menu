0a8ed4f5788d6c789409cac12884e521faaf53b3
firoliyos <firatesfaye44@gmail.com>
2024-10-21 09:43:30 +0300

    menu-item rendered

--- recursive-nav-menu/src/App.js
+++ recursive-nav-menu/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-
+import data from "./components/data"
+import './App.css'
+import TreeView from "./components/index"
+import menus from "./components/data"
 function App() {
   return (
     <div className="App">
-      Hello
+      <TreeView menus={menus} />
     </div>
   );
 }
